5ab5d5d1adeab571d15e961e847e3723fa71c414
YuriBY <vv0325056@gmail.com>
2023-06-21 12:20:24 +0300

    Couters for columns

--- components/button/addToDo.js
+++ components/button/addToDo.js
@@ -1,4 +1,5 @@
 import { timestamp } from "../time/time.js";
+import { countCards } from "../../main.js";
 
 // const addTodo = document.getElementById('addTodo');
 // addTodo.addEventListener('click', (event) => {
@@ -34,13 +35,14 @@ function createTodo(id, title, desc, user, date) {
     const todoColumn = document.getElementById('todoColumn');
     const newTodo = returnHTML(id, title, desc, user, date);
     todoColumn.innerHTML =  todoColumn.innerHTML + newTodo;
-    addLocalstorageTodo(id, title, desc, user, timestamp.innerHTML)
+    addLocalstorageTodo(id, title, desc, user, timestamp.innerHTML);
+    countCards();
+   
 
 };
 
 function returnHTML(id, title, desc, user, date) {
-    console.log('ReturnHTML');
-    return  ` <div class="card" id="${id}">
+   return  ` <div class="card" id="${id}">
                   
     <div class="card-top-buttons">
        <div class="button" id="some-id"><a href="#openModal">Edit</a></div>
@@ -78,13 +80,13 @@ function addLocalstorageTodo (id, title, desc, user, date) {
     localStorage.setItem('trelloKey', JSON.stringify(localStorageArr));   
 };
 
-export function renderTodo() {
-    console.log('Hello');
+export function renderTodo() {    
     const localStorageArr = JSON.parse(localStorage.getItem('trelloKey'));
     localStorageArr[0].forEach(element => {      
        const todoColumn = document.getElementById('todoColumn');
        const newTodo = returnHTML(element.id, element.title, element.desc, element.user, element.date);
        todoColumn.innerHTML =  todoColumn.innerHTML + newTodo;
+       countCards();
     });
 
 }
--- main.js
+++ main.js
@@ -7,7 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('trelloKey', JSON.stringify([[], [], []]));
     };  
     renderTodo();      
-})
+});
+
+export function countCards() {
+    const countColumns = document.querySelectorAll('.cards-wrapper');
+    const countHTMLFirstColumn = countColumns[0].querySelectorAll('.card');
+    const countHTMLSecondColumn = countColumns[1].querySelectorAll('.card');
+    const countHTMLThirdColumn = countColumns[2].querySelectorAll('.card');
+    const countFirstColumn = Array.from(countHTMLFirstColumn).length;
+    const countSecondColumn = Array.from(countHTMLSecondColumn).length;
+    const countThirdtColumn = Array.from(countHTMLThirdColumn).length;
+    document.getElementById('todocounter').textContent = `${countFirstColumn}`;
+    document.getElementById('in-progress-counter').textContent = `${countSecondColumn}`;
+    document.getElementById('done-counter').textContent = `${countThirdtColumn}`;
+};
+countCards();
+
+
 
 
 
