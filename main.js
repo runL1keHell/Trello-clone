59c00abc299ae35ea9006be78579baa096006f92
runL1keHell <oleg204grishan@gmail.com>
2023-06-29 18:18:59 +0300

    moved 'moveTodo.js' to button-move directory (and changed paths)

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import './components/localStorage/localStorage.js';
 // import './components/button/addToDo.js';
 import './components/button/editTodo.js';
 import './components/button/deleteTodo.js';
-import './components/button/moveTodo.js';
+import './components/button-move/moveTodo.js';
 import './components/users/users.js';
 
 // import {ACTIONS, BUTTON_TITLES, CARD_FIELDS, CARDS_LIMIT_INPROGRESS, CARD_COLORS, ERROR_TITLE, WARNING_LIMIT_INPROGRESS, WARNING_DELETE_ALL_CARDS}  from "./constants/constants.js"
