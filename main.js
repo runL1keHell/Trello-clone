c8fe22f21dbfa88f83d71e0ae4874a04d1d40923
AlexeyKidA <zhuraulioualiaksei@gmail.com>
2023-07-02 17:14:52 +0300

    Deploy test

--- main.js
+++ main.js
@@ -10,4 +10,4 @@ import './components/button-move/moveTodo.js';
 import './components/users/users.js';
 
 // import {ACTIONS, BUTTON_TITLES, CARD_FIELDS, CARDS_LIMIT_INPROGRESS, CARD_COLORS, ERROR_TITLE, WARNING_LIMIT_INPROGRESS, WARNING_DELETE_ALL_CARDS}  from "./constants/constants.js"
-// константы (тексты кнопок и ошибки, цвета, лимиты)
+// константы (тексты кнопок и ошибки, цвета, лимиты).
